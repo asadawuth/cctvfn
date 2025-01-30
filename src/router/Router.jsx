36c1c4b3c1b20bd48b54cfe931746aae4bd3ec0d
asadawuth <taodewy@gmail.com>
2025-01-30 15:43:00 +0700

    dwd

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -38,7 +38,7 @@ import DataRequestPass from "../layoutconponent/DashboardAllValue/dataRequestCct
 import DataRequestNotpass from "../layoutconponent/DashboardAllValue/dataRequestCctv/DataRequestNotpass";
 import SosFormMobile from "../page/Inuserreport/SosFormMobile";
 import Reported from "../layoutconponent/DashboardAllValue/total5sosvoicestatus/Reported";
-import Acknowledged from "../layoutconponent/DashboardAllValue/total5sosvoicestatus/acknowledged";
+import Acknowledged from "../layoutconponent/DashboardAllValue/total5sosvoicestatus/Acknowledged";
 import InProgress from "../layoutconponent/DashboardAllValue/total5sosvoicestatus/InProgress";
 import Completed from "../layoutconponent/DashboardAllValue/total5sosvoicestatus/Completed";
 import Canceled from "../layoutconponent/DashboardAllValue/total5sosvoicestatus/Canceled";
