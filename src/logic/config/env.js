7b11b693f75a56695db09454da674f94536faba9
asadawuth <taodewy@gmail.com>
2025-04-02 16:45:27 +0700

    dwdwfgw

--- src/logic/config/env.js
+++ src/logic/config/env.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "env-8549838.proen.app.ruk-com.cloud";
+export const BACKEND_URL = "http://192.168.1.122:8888"
+// "env-8549838.proen.app.ruk-com.cloud";
 // "http://localhost:8888"
 // "https://mass-serv.ddns.net"
 // "https://nodeapi.masscorporation.site/"
