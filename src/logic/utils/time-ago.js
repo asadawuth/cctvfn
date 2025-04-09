19118a641c9b5e318a6e20ba166dbf654b8633e3
asadawuth <taodewy@gmail.com>
2025-04-09 15:33:27 +0700

    dwdwdw

--- src/logic/utils/time-ago.js
+++ src/logic/utils/time-ago.js
@@ -1,8 +1,22 @@
+// import TimeAgo from "javascript-time-ago";
+// import th from "javascript-time-ago/locale/th";
+
+// TimeAgo.addLocale(th);
+// const timeAgo = new TimeAgo("th-TH");
+
+// const formatTimeAgo = (date) => timeAgo.format(new Date(date), "mini-now");
+// export default formatTimeAgo;
+
 import TimeAgo from "javascript-time-ago";
 import th from "javascript-time-ago/locale/th";
+import { DateTime } from "luxon";
 
 TimeAgo.addLocale(th);
 const timeAgo = new TimeAgo("th-TH");
 
-const formatTimeAgo = (date) => timeAgo.format(new Date(date), "mini-now");
+const formatTimeAgo = (date) => {
+  const bangkokTime = DateTime.fromISO(date, { zone: "Asia/Bangkok" });
+  return timeAgo.format(bangkokTime.toJSDate(), "mini-now");
+};
+
 export default formatTimeAgo;
