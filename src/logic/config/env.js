055e2380f59712c2b6d363fe5c5c9732b226dbd7
asadawuth <taodewy@gmail.com>
2025-04-10 13:16:40 +0700

    ifhei

--- src/logic/config/env.js
+++ src/logic/config/env.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:8888";
+export const BACKEND_URL = "https://env-8549838.proen.app.ruk-com.cloud";
 // "http://localhost:8888"
 // "https://env-8549838.proen.app.ruk-com.cloud"
