5c9efc4e2b3a9c6d85db4ac9d2f2b641c545247a
asadawuth <122080707+asadawuth@users.noreply.github.com>
2025-08-25 15:33:39 +0700

    15.33

--- src/logic/config/env.js
+++ src/logic/config/env.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://192.168.2.55:8888";
+export const BACKEND_URL = "http://192.168.2.55";
 // "http://localhost:8888"
 // "https://env-8549838.proen.app.ruk-com.cloud"
 // "https://teetest.shop"
