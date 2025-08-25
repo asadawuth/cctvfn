ccda69f8e1ef1a77e64a14534e6723a79cbc8ccc
asadawuth <122080707+asadawuth@users.noreply.github.com>
2025-08-25 16:58:16 +0700

    16.58

--- src/logic/config/env.js
+++ src/logic/config/env.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "http://182.53.88.40:8888";
+export const BACKEND_URL = "https://ce6ffd696d0d.ngrok-free.app";
 // "http://localhost:8888"
 // "https://env-8549838.proen.app.ruk-com.cloud"
 // "https://teetest.shop"
+// "http://182.53.88.40:8888"
